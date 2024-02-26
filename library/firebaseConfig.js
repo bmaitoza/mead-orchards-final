823473c0d177ddfdc4cb4d8ebff174c32f862f45
bmaitoza <156103153+bmaitoza@users.noreply.github.com>
2024-02-26 18:45:16 -0500

    deleted sign up js

--- library/firebaseConfig.js
+++ library/firebaseConfig.js
@@ -1,21 +1,25 @@
-import { initializeApp } from "firebase/App";
-import { getFirestone } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBwXXoNPLO4X5reYFqXB7_llsdGUZBGkCY",
-    authDomain: "x",
-    projectId: "mead-orchards",
-    storageBucket: "x",
-    messagingSenderId: "x",
-    appId: "x",
-    mesurementId: "x"
-}
+  apiKey: "AIzaSyBwXXoNPLO4X5reYFqXB7_llsdGUZBGkCY",
+  authDomain: "mead-orchards.firebaseapp.com",
+  projectId: "mead-orchards",
+  storageBucket: "mead-orchards.appspot.com",
+  messagingSenderId: "212384900214",
+  appId: "1:212384900214:web:fda96aedd5b04402feeb57",
+  measurementId: "G-ZQG7CE0ERJ"
+};
 
-//Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
-export const database = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider(app).addScope('email');
-export default app
